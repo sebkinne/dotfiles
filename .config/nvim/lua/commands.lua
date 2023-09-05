4ca0c36717684690058aae5da7ce670683eb069c
Sebastian <contact@sebkinne.com>
2023-09-05 22:18:16 +1000

    nvim/commands: fix LUA errors and add more workspace commands
    The additional workspace commands are to create new workspaces:
    Wsn and Wsnf (fig)

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -2,13 +2,15 @@
 local ok, google = pcall(require, 'google')
 if ok then
     -- CS
-    vim.api.nvim_create_user_command('Cs', google.cs_query(), {})
-    vim.api.nvim_create_user_command('Cse', google.cs_query_exp(), {})
-    vim.api.nvim_create_user_command('Cf', google.cs_file(), {})
-    vim.api.nvim_create_user_command('Cfe', google.cs_file_exp(), {})
+    vim.api.nvim_create_user_command('Cs', google.cs_query, {})
+    vim.api.nvim_create_user_command('Cse', google.cs_query_exp, {})
+    vim.api.nvim_create_user_command('Cf', google.cs_file, {})
+    vim.api.nvim_create_user_command('Cfe', google.cs_file_exp, {})
 
     -- Workspaces
-    vim.api.nvim_create_user_command('Ws', google.pick_ws(), {})
+    vim.api.nvim_create_user_command('Ws', google.pick_ws, {})
+    vim.api.nvim_create_user_command('Wsn', google.new_ws, {nargs = 1})
+    vim.api.nvim_create_user_command('Wsnf', google.new_ws_fig, {nargs = 1})
 end
 
 
